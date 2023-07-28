694b58ffc7937b4642dafe67755edb97d3dc203e
sagapov <sagapov@smuit.ru>
2023-07-28 17:45:42 +0400

    first commit

---
+++ Droid.cs
@@ -0,0 +1,29 @@
+public class Droid {
+
+public float x;
+public float y;
+public float speed;
+
+public Droid () {
+
+}
+
+public void Navigate(float x, float y){
+
+}
+
+
+public (float x, float y) Position() {
+    return (x, y);
+}
+
+public float Speed(){
+    return speed;
+}
+
+
+public GetNearObjects(float x, float y) {
+    return objects
+}
+
+}
---
+++ SRP.cs
@@ -0,0 +1,25 @@
+
+// Employee 
+public class Employee {
+    //busness responsibility
+    public float CalculatePay();
+
+    //system responsibility
+    public SaveToDB();
+}
+
+
+// IOT class
+public class IOT {
+    // connection responsibility
+    public Connect(string ID);
+    public Disconnect();
+
+    //communication responsibility
+    public Send(string command);
+    public string Recieve();
+}
+
+
+
+
