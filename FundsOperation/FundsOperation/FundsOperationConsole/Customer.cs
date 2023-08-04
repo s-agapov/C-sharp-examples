bd6ed92a02b6a576be5859f12aacddd85e52cbea
sagapov <sagapov@smuit.ru>
2023-08-04 16:07:31 +0400

    Customer class

---
+++ FundsOperation/FundsOperation/FundsOperationConsole/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundsOperationConsole
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string PIN { get; set; }
+
+        public bool validatePIN(string pin)
+        {
+            if (pin == PIN) { return true; }
+
+            else { return false; }
+        }
+    }
+}
